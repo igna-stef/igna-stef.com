6a2f53c4fe66d6338ee8eb8bd375f209f2ca4d11
Ignacio Stefanini <127454179+igna-stef@users.noreply.github.com>
2023-04-27 17:03:16 -0300

    initial commit

---
+++ assets/contact.php
@@ -0,0 +1,56 @@
+<?php
+
+$errorMSG = "";
+
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+
+
+$EmailTo = "stefanini.ignacio@gmail.com";
+$Subject = "Igna-Stef.com contacto directo";
+
+// prepare email body text
+$Body = "";
+$Body .= "Nombre: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Mensaje: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "enviado exitosamente";
+}else{
+    if($errorMSG == ""){
+        echo "No se pudo enviar :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+?>
